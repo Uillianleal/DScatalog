bcf9996b89247d810d2589543b9acd83b0be8073
Uillianleal <willianleal52@hotmail.com>
2022-01-19 16:37:32 -0300

    Backend project created

---
+++ backend/src/main/java/com/devsuperior/DScatalog/DScatalogApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DScatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DScatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DScatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/DScatalog/DScatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DScatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DScatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
