9ae773ea502ed2bc038e2db1d0e11c6d3155be17
Uillianleal <willianleal52@hotmail.com>
2022-04-12 11:21:25 -0300

    First upload test

--- backend/src/main/java/com/devsuperior/DScatalog/DScatalogApplication.java
+++ backend/src/main/java/com/devsuperior/DScatalog/DScatalogApplication.java
@@ -1,13 +1,26 @@
 package com.devsuperior.DScatalog;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DScatalogApplication {
+import com.devsuperior.DScatalog.services.S3Service;
 
+@SpringBootApplication
+public class DScatalogApplication implements CommandLineRunner {
+
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DScatalogApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		s3Service.uploadFile("G:\\imagem-test\\test.jpg");
+		
 	}
 
 }
