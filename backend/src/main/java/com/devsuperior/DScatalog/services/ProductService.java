f407cdec7437f8b8bb357e8c08922769981fc16b
Uillianleal <willianleal52@hotmail.com>
2022-04-12 12:25:13 -0300

    Upload endpoint

--- backend/src/main/java/com/devsuperior/DScatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/DScatalog/services/ProductService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.DScatalog.services;
 
+import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -13,9 +14,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.devsuperior.DScatalog.dto.CategoryDTO;
 import com.devsuperior.DScatalog.dto.ProductDTO;
+import com.devsuperior.DScatalog.dto.UriDTO;
 import com.devsuperior.DScatalog.entities.Category;
 import com.devsuperior.DScatalog.entities.Product;
 import com.devsuperior.DScatalog.repositories.CategoryRepository;
@@ -31,6 +34,9 @@ public class ProductService {
 
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private S3Service s3Service;
 
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> findAllPaged(Long categoryId, String name, Pageable pageable) {
@@ -93,5 +99,10 @@ public class ProductService {
 			entity.getCategories().add(category);
 		}
 
+	}
+
+	public UriDTO uploadFile(MultipartFile file) {
+		URL url = s3Service.uploadFile(file);
+		return new UriDTO(url.toString());
 	}
 }
