91385a6992c705c4b2881794230732ab5acb3a0c
Uillianleal <willianleal52@hotmail.com>
2022-07-12 11:16:25 -0300

    ProductDatails layout mobile

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,7 +1,35 @@
+import { ReactComponent as ArroIcon } from 'assets/images/arrow.svg';
+import ProductPrice from 'components/ProductPrice';
+import './styles.css'
+
 const ProductDetails = () => {
-    return (
-        <h1>Pagina ProductDetails</h1>
-    );
-}
+  return (
+    <div className="product-details-container">
+      <div className="base-card product-datails-card">
+        <div className="goback-container">
+            <ArroIcon/>
+            <h2>VOLTAR</h2>
+        </div>
+        <div className="row">
+            <div className="col-xl-6">
+                <div className="img-container">
+                    <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt="Nome do Produto" />
+                </div>
+                <div className="name-price-container">
+                    <h1>Nome do produto</h1>
+                    <ProductPrice price={2345.67}/>
+                </div>
+            </div>
+            <div className="col-xl-6">
+                <div className="description-container">
+                    <h2>Descrição do produto</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, officia?</p>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ProductDetails;
