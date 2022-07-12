1f9b6b33fd14793c4d696373caf2b509c58ecc22
Uillianleal <willianleal52@hotmail.com>
2022-07-12 11:43:21 -0300

    Catalog - ProductDetails links

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,31 +1,40 @@
 import { ReactComponent as ArroIcon } from 'assets/images/arrow.svg';
 import ProductPrice from 'components/ProductPrice';
-import './styles.css'
+import { Link } from 'react-router-dom';
+import './styles.css';
 
 const ProductDetails = () => {
   return (
     <div className="product-details-container">
       <div className="base-card product-datails-card">
-        <div className="goback-container">
-            <ArroIcon/>
+        <Link to="/products">
+          <div className="goback-container">
+            <ArroIcon />
             <h2>VOLTAR</h2>
-        </div>
+          </div>
+        </Link>
         <div className="row">
-            <div className="col-xl-6">
-                <div className="img-container">
-                    <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt="Nome do Produto" />
-                </div>
-                <div className="name-price-container">
-                    <h1>Nome do produto</h1>
-                    <ProductPrice price={2345.67}/>
-                </div>
+          <div className="col-xl-6">
+            <div className="img-container">
+              <img
+                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
+                alt="Nome do Produto"
+              />
             </div>
-            <div className="col-xl-6">
-                <div className="description-container">
-                    <h2>Descrição do produto</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, officia?</p>
-                </div>
+            <div className="name-price-container">
+              <h1>Nome do produto</h1>
+              <ProductPrice price={2345.67} />
             </div>
+          </div>
+          <div className="col-xl-6">
+            <div className="description-container">
+              <h2>Descrição do produto</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo,
+                officia?
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
