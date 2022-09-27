85948a3917bcd34370cdff734abdba842670e2e0
Uillian Leal <willianleal52@hotmail.com>
2022-09-27 12:04:31 -0300

    MSW mock server, fixtures

--- frontweb/src/pages/Catalog/__tests__/Catalog.spec.tsx
+++ frontweb/src/pages/Catalog/__tests__/Catalog.spec.tsx
@@ -2,18 +2,21 @@ import { render, screen, waitFor } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 import history from 'util/history';
 import Catalog from '..';
+import { server } from './fixtures';
 
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 test('should render Catalog with products', async () => {
-    render(
-        <Router history={history}>
-            <Catalog />
-        </Router>
-    );
+  render(
+    <Router history={history}>
+      <Catalog />
+    </Router>
+  );
+  expect(screen.getByText('Catálogo de produtos')).toBeInTheDocument();
 
-    expect(screen.getByText('Catálogo de produtos')).toBeInTheDocument();
-
-    await waitFor(() => {
-        expect(screen.getByText('Smart TV')).toBeInTheDocument();
-    });
+  await waitFor(() => {
+    expect(screen.getByText('Smart TV')).toBeInTheDocument();
+  });
 });
